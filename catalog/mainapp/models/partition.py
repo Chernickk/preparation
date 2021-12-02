2bba57007adf79946f02f31d64b08d7a217badd7
nik <17rolex@gmail.com>
2021-12-02 10:19:48 +0300

    lesson6

--- catalog/mainapp/models/partition.py
+++ catalog/mainapp/models/partition.py
@@ -1,8 +1,13 @@
+from django.contrib.sites.managers import CurrentSiteManager
 from django.db import models
 from .good import Good
+from django.contrib.sites.models import Site
 
 
 class Partition(models.Model):
     name = models.CharField(max_length=128)
     description = models.TextField(blank=True)
     goods = models.ManyToManyField(Good, related_name='partitions')
+    site = models.ForeignKey(Site, on_delete=models.CASCADE)
+    objects = models.Manager()
+    on_site = CurrentSiteManager()
--- catalog/mainapp/views.py
+++ catalog/mainapp/views.py
@@ -4,10 +4,10 @@ from .models import Good, Partition
 
 class ListGoods(ListView):
     model = Good
-    queryset = Good.objects.prefetch_related('partitions')
-    extra_context = {'partitions': Partition.objects.all()}
+    queryset = Good.on_site.prefetch_related('partitions')
+    extra_context = {'partitions': Partition.on_site.all()}
 
 
 class PartitionView(DetailView):
     model = Partition
-    queryset = Partition.objects.all().prefetch_related('goods', 'goods__partitions')
+    queryset = Partition.on_site.all().prefetch_related('goods', 'goods__partitions')
